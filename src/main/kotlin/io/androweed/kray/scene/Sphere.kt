f37b5523bdb4e59750716125dfb6d100fd32e62f
Aurelman <just-me@anywhere.com>
2018-06-11 23:36:26 +0200

    Added sphere scene objects

---
+++ src/main/kotlin/io/androweed/kray/scene/Sphere.kt
@@ -0,0 +1,26 @@
+package io.androweed.kray.scene
+
+import io.androweed.kray.core.Position
+import io.androweed.kray.core.QuadraticEquation
+import io.androweed.kray.core.Ray
+
+data class Sphere(private val center: Position, private val radius: Double): Primitive {
+    override fun normalAt(position: Position) = (center toward position).normalize()
+
+    override fun intersect(ray: Ray): IntersectionResult {
+        val positionToRayOrigin = center toward ray.origin
+        val a = ray.direction.squaredLength
+        val b = 2 * (ray.direction dot positionToRayOrigin)
+        val c = positionToRayOrigin.squaredLength - squaredRadius
+        val result = QuadraticEquation(a, b, c)
+        if (result.hasNoRoot()) {
+            return IntersectionResult(this, false, null)
+        }
+        val intersectionPosition = ray.origin + (ray.direction.normalize() * result.firstRoot())
+        return IntersectionResult(this, true, intersectionPosition)
+    }
+
+    private val squaredRadius by lazy {
+        radius * radius
+    }
+}
