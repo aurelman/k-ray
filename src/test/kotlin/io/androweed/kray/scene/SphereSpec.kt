dda6a6b760420024bcf1f23f64d8635a617baf36
Aurelman <just-me@anywhere.com>
2019-05-22 15:26:50 +0200

    migrated test frameworks + added gradle-wrapper.jar

--- src/test/kotlin/io/androweed/kray/scene/SphereSpec.kt
+++ src/test/kotlin/io/androweed/kray/scene/SphereSpec.kt
@@ -1,15 +1,15 @@
 package io.androweed.kray.scene
 
+import assertk.assertThat
+import assertk.assertions.isCloseTo
+import assertk.assertions.isFalse
+import assertk.assertions.isTrue
 import io.androweed.kray.core.Position
 import io.androweed.kray.core.Ray
 import io.androweed.kray.core.Vector
-import io.androweed.main.io.androweed.kray.scene.Sphere
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.offset
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
 
 internal object SphereSpec : Spek({
 
@@ -19,7 +19,7 @@ internal object SphereSpec : Spek({
                 center = Vector(0.0, 0.0, 0.0),
                 radius = 10.0)
 
-        on("testing intersections") {
+        context("testing intersections") {
 
             it("should detect no intersection if none") {
                 val ray = Ray(origin = Vector(100.0, 100.0, 100.0), direction = Vector(12.0, 25.0, 100.0))
@@ -31,13 +31,13 @@ internal object SphereSpec : Spek({
                 val ray  = Ray(origin = Position(x = 0.0, y = 10.0, z = -100.0), direction = Vector(x= 0.0, y = 0.0, z = 1.0))
                 val result = sphere.intersect(ray)
                 assertThat(result.intersect).isTrue()
-                assertThat(result.intersection?.x).isCloseTo(0.0, offset(0.000001))
-                assertThat(result.intersection?.y).isCloseTo(10.0, offset(0.000001))
-                assertThat(result.intersection?.z).isCloseTo(0.0, offset(0.000001))
+                assertThat(result.intersection?.x as Double).isCloseTo(0.0, 0.000001)
+                assertThat(result.intersection?.y as Double).isCloseTo(10.0, 0.000001)
+                assertThat(result.intersection?.z as Double).isCloseTo(0.0, 0.000001)
 
-                assertThat(result.normal.x).isCloseTo(0.0, offset(0.000001))
-                assertThat(result.normal.y).isCloseTo(1.0, offset(0.000001))
-                assertThat(result.normal.z).isCloseTo(0.0, offset(0.000001))
+                assertThat(result.normal.x).isCloseTo(0.0, 0.000001)
+                assertThat(result.normal.y).isCloseTo(1.0, 0.000001)
+                assertThat(result.normal.z).isCloseTo(0.0, 0.000001)
             }
 
             it("should detect intersection with a traversing ray") {
@@ -46,13 +46,13 @@ internal object SphereSpec : Spek({
                 val result = sphere.intersect(ray)
 
                 assertThat(result.intersect).isTrue()
-                assertThat(result.intersection?.x).isCloseTo(0.0, offset(0.000001))
-                assertThat(result.intersection?.y).isCloseTo(0.0, offset(0.000001))
-                assertThat(result.intersection?.z).isCloseTo(-10.0, offset(0.000001))
+                assertThat(result.intersection?.x as Double).isCloseTo(0.0, 0.000001)
+                assertThat(result.intersection?.y as Double).isCloseTo(0.0, 0.000001)
+                assertThat(result.intersection?.z as Double).isCloseTo(-10.0, 0.000001)
 
-                assertThat(result.normal.x).isCloseTo(0.0, offset(0.000001))
-                assertThat(result.normal.y).isCloseTo(0.0, offset(0.000001))
-                assertThat(result.normal.z).isCloseTo(-1.0, offset(0.000001))
+                assertThat(result.normal.x).isCloseTo(0.0, 0.000001)
+                assertThat(result.normal.y).isCloseTo(0.0, 0.000001)
+                assertThat(result.normal.z).isCloseTo(-1.0, 0.000001)
             }
         }
     }
